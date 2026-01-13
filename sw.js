ae9c2ea36e4e44c7df67b0e0c2a09016b1c82308
Gonza-wf <121890733+Gonza-wf@users.noreply.github.com>
2026-01-13 00:37:06 -0300

    Refactor service worker for caching and activation

--- sw.js
+++ sw.js
@@ -1,18 +1,32 @@
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("battery-baja").then(cache =>
-      cache.addAll([
-        "./",
-        "./index.html",
-        "./style.css",
-        "./game.js"
-      ])
-    )
+const CACHE_NAME = "bateria-baja-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./game.js",
+  "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(res => res || fetch(event.request))
   );
 });
